d817afc2e59daac9bffd3ee55c6872ea213e258a
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-01-27 09:23:10 +0200

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if(env("APP_ENV")!="local"){
-          \Illuminate\Support\Facades\URL::forceScheme("https");
+          URL::forceScheme("https");
         }
     }
 }
